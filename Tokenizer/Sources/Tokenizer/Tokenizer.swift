f0e9c20684bb0e96a1c67113fe3a279d33940c04
Simon Ferns <122154257+AdonisCodes@users.noreply.github.com>
2025-04-23 18:09:01 -0600

    simon/simple swift tokenizer tests (#8)
    * feat: implement initial tests
    
    * feat: add tokenizer test suite
    
    * fix test paths
    
    * feat: remove variable declarations for now
    
    * feat: add error case testing
    
    * fix: refactor file names
    
    * feat: add more edge-case tests

--- Tokenizer/Sources/Tokenizer/Tokenizer.swift
+++ Tokenizer/Sources/Tokenizer/Tokenizer.swift
@@ -8,7 +8,7 @@ public enum TokenizerError: Error {
     case invalidTokenProcessorLiteralLengthGreaterThan0
 }
 
-public enum TokenType {
+public enum TokenType: Sendable {
     case string
     case addition, subtraction, multiplication, division, modulus, exponentiation, power
     case number
@@ -17,7 +17,7 @@ public enum TokenType {
 }
 
 
-public struct Token {
+public struct Token: Equatable, Sendable {
     public let literal: String
     public let type: TokenType
     public let lineStart: Int
@@ -32,28 +32,28 @@ public class Tokenizer {
     var current: Int = 0
     var start:  Int = 0
     var line: Int = 1
-    
+
     let reservedKeywords = [
         "var": TokenType.variable
     ]
-    
+
     public init(source: String) {
         self.source = source
     }
-    
+
     public func scanTokens() throws -> [Token] {
         current = 0
         while !isAtEnd() {
             start = current
             try scanToken()
         }
-        
+
         return tokens
     }
-    
+
     func scanToken() throws {
         let literal = advance()
-        
+
         switch literal {
             case "\n":
                 line += 1
@@ -87,33 +87,33 @@ public class Tokenizer {
                 throw TokenizerError.invalidCharacter
         }
     }
-    
+
     @discardableResult
     func advance() -> Character {
         if isAtEnd() {
             return "\0"
         }
-        
+
         let index = source.index(source.startIndex, offsetBy: current)
-        
+
         current += 1
-        
+
         return source[index]
     }
-    
+
     func peek() -> Character {
         if isAtEnd() {
             return "\0"
         }
-        
+
         let index = source.index(source.startIndex, offsetBy: current)
         return source[index]
     }
-    
+
     func isAtEnd() -> Bool {
         return current >= source.count
     }
-    
+
     func addToken(type tokenType: TokenType, literal: String, startLine: Int) {
         let token = Token(
             literal: literal,
@@ -125,12 +125,12 @@ public class Tokenizer {
         )
         tokens.append(token)
     }
-    
+
     func string() throws {
         var literal: String = ""
         var isQuoted: Bool = false
         let startLine: Int = line
-        
+
         while !isAtEnd() {
             let character = advance()
             // This adds support for multiline strings
@@ -139,11 +139,11 @@ public class Tokenizer {
                 if !literal.isEmpty && !isBeforeQuoted {
                     literal += "\n"
                 }
-                
+
                 line += 1
                 continue
             }
-            
+
             // This allows us to add support for escaping characters as well!
             if character == "\\" {
                 let nextCharacter = peek()
@@ -164,7 +164,7 @@ public class Tokenizer {
                 }
                 continue
             }
-            
+
             if character != "\"" {
                 literal = "\(literal)\(character)"
             } else {
@@ -172,7 +172,7 @@ public class Tokenizer {
                 break
             }
         }
-        
+
         if isQuoted {
             addToken(
                 type: .string,
@@ -183,11 +183,11 @@ public class Tokenizer {
             throw TokenizerError.unterminatedString
         }
     }
-    
+
     func isDigit(_ character: Character) -> Bool {
         return character.isNumber
     }
-    
+
     func number(first: String) throws {
         var number = first
         while !isAtEnd() {
@@ -197,47 +197,47 @@ public class Tokenizer {
                 line += 1
                 break
             }
-            
+
             if next == " " {
                 advance()
                 break
             }
-            
+
             if !isDigit(next) && next != "." {
                 break
             }
-            
+
             let current = advance()
-            
+
             if current == "." && number.contains(".") {
                 throw TokenizerError.invalidNumber
             }
-            
+
             if current == "." && isDigit(peek()) {
                 number += "."
             } else {
                 number = "\(number)\(current)"
             }
         }
-        
+
         guard Double(number) != nil else {
             throw TokenizerError.invalidNumber
         }
-        
+
         addToken(type: .number, literal: number, startLine: line)
     }
-    
+
     func identifier(first: Character) throws {
         var literal: String = "\(first)"
-        
+
         while !isAtEnd() {
             if !isAlphaNumeric(peek()) {
                 break
             }
-            
+
             literal = "\(literal)\(advance())"
         }
-        
+
         if let reservedKeywordType = reservedKeywords[literal] {
             addToken(
                 type: reservedKeywordType,
@@ -248,7 +248,7 @@ public class Tokenizer {
             addToken(type: .identifier, literal: literal, startLine: line)
         }
     }
-    
+
     func isAlphaNumeric(_ character: Character) -> Bool {
         return character.isLetter || character.isNumber
     }
