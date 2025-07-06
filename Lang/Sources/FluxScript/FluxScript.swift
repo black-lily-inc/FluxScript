39a7e72d09124f4d9d42c6417e80674a85f74b75
AdonisCodes <business@simonferns.com>
2025-07-06 15:55:44 -0600

    fix: Make lang compile actually

--- Lang/Sources/FluxScript/FluxScript.swift
+++ Lang/Sources/FluxScript/FluxScript.swift
@@ -4,9 +4,8 @@ import AST
 
 public struct FluxScript {
     public init() {}
-    
+
     public func run(input: String) throws {
         let tokens = try Tokenizer(source: input).scanTokens()
-        let ast = try AST
     }
 }
